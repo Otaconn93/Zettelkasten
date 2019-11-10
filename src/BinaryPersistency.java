b8ca4bf75379ce985de2dd73e9d90bdb8bec7f4d
Shamil Gradov <shamil.gradov@hochschule-stralsund.de>
2019-11-10 19:43:27 +0100

    load und save binary

--- src/BinaryPersistency.java
+++ src/BinaryPersistency.java
@@ -1,11 +1,30 @@
+import java.io.*;
+
 public class BinaryPersistency implements Persistency {
 
     @Override
     public void save(Zettelkasten zk, String dateiname) {
+        try {
+            FileOutputStream fos = new FileOutputStream(dateiname+".ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(zk);
+            oos.close();
+            fos.close();
+        }catch (IOException ioe){
+            System.out.println("Datei konnte nicht erstellt werden");
+        }
 
     }
     @Override
     public Zettelkasten load(String dateiname) {
+        try{
+            FileInputStream fis = new FileInputStream(dateiname);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Zettelkasten datei = (Zettelkasten) ois.readObject();
+            return datei;
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("File konnte nicht gefunden werden");
+        }
         return null;
     }
 }
