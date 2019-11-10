daef998e4251dcc8adcc697c3bdfa12ef5b9365e
Shamil Gradov <shamil.gradov@hochschule-stralsund.de>
2019-11-10 08:52:34 +0100

    Zettelkasten Methoden hinzugefügt

--- src/Zettelkasten.java
+++ src/Zettelkasten.java
@@ -2,9 +2,35 @@ import java.util.ArrayList;
 
 public class Zettelkasten {
     private ArrayList<Medium> myZettelkasten = new ArrayList<Medium>();
+    private boolean sortiert = false;
 
-    addMedium();
-    DropMedium();
-    FindMedium();
-    Sort();
+    public void addMedium(Medium medium){
+        myZettelkasten.add(medium);
+        sortiert = false;
+    }
+    public void dropMedium(String parameter){
+        for (Medium m: myZettelkasten) {
+            if(m.getTitel().contains(parameter)){
+                myZettelkasten.remove(m);
+                break;
+            }
+        }
+    }
+    public Medium findMedium(String _titel){
+        for(Medium medium : myZettelkasten){
+            if(medium.getTitel().contains(_titel)){
+                return medium;
+            }
+        }
+        return null;
+    }
+
+    public void sort(){
+        if(sortiert==true){
+            System.out.println("Ist bereits sortiert");
+        }else{
+            //Wie sortier ich den bumms
+            sortiert=true;
+        }
+    }
 }
